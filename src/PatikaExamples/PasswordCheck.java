3ed2298f9f0c676f5c18ec3bb31a8bb8be2f4fe3
Bekir Yenidoğan <bekiryenidogan16@gmail.com>
2022-07-23 14:46:51 +0300

    PasswordCheck Patika

--- src/PatikaExamples/PasswordCheck.java
+++ src/PatikaExamples/PasswordCheck.java
@@ -13,20 +13,25 @@ public class PasswordCheck {
         System.out.println("Please enter your password");
         checkpassword = kb.nextLine();
 
-        if (password.equals(checkpassword)) {
+        if (password.equals(checkpassword))
             System.out.println("Password Correct");
-        }else if (!password.equals(checkpassword)){
-            System.out.println("Password is wrong do you want set a new password ? 1-Yes, 0- No");
-            choice= kb.nextInt();
-            if (choice == 1) {
-                System.out.println("Define your new password: ");
-                newpassword= kb.next();
-                if (newpassword.equals(password))
-                    System.out.println("Your password must be different from your last password");
-                else {
-                    System.out.println("Your password is changed");
-                }
+        else if(!password.equals(checkpassword))
+            System.out.println("Password is wrong do yo want set a new password 1/0");
+        choice = kb.nextInt();
+        if (choice == 1){
+            System.out.println("Define new password");
+            newpassword = kb.next();
+            if (newpassword.equals(password)){
+                System.out.println("Your password must be different from your last password.");
+            } else{
+                System.out.println("Your password is changed");
+                System.out.printf("New password is %s", newpassword);
             }
+
+        } else {
+            System.out.println("Returning main page...");
         }
+
+
     }
 }
