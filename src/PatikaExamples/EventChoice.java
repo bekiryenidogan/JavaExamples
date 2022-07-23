35cecc6f19825c2ed9ff263db5980d83bf000153
Bekir Yenidoğan <bekiryenidogan16@gmail.com>
2022-07-23 15:04:09 +0300

    EventChoice Patika

---
+++ src/PatikaExamples/EventChoice.java
@@ -0,0 +1,26 @@
+package PatikaExamples;
+
+public class EventChoice {
+    public static void main(String[] args) {
+        int heat;
+
+        java.util.Scanner kb = new java.util.Scanner(System.in);
+
+        System.out.println("Degree");
+
+        heat = kb.nextInt();
+
+        if (heat <= 5){
+            System.out.println("Ski");
+        } else if ((heat>5)&&(heat <= 15)){
+            System.out.println("Cinema");
+        } else if ((heat > 15)&&(heat <=25)){
+            System.out.println("Picnic");
+        }else
+            System.out.println("Swimming");
+
+
+
+
+    }
+}
